864312301a29388d07689a2487833a183033968e
Blair Wright <blairbwright@gmail.com>
2015-02-26 15:04:10 -0800

    new

---
+++ view/navigation.php
@@ -0,0 +1,9 @@
+<?php
+    require_once(__DIR__ . "/../model/config.php");
+    //this is where my website navigates.
+?>
+<nav>
+    <u1>
+        <li><a href="<?php echo $path . "post.php" ?>">Blog Post Form</a><li>
+    </u1>
+</nav>
